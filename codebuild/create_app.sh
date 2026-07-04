14950329d51aca48cf555bda83551d184d36d22d
rhishikesh-chikhalkar <rhishichikhalkar21@gmail.com>
2026-07-04 23:23:09 +0530

    chore: disable proxy buffering and caching with extended timeout in Nginx configuration

--- codebuild/create_app.sh
+++ codebuild/create_app.sh
@@ -321,6 +321,9 @@ else
     proxy_set_header X-Real-IP \$remote_addr;
     proxy_set_header X-Forwarded-For \$proxy_add_x_forwarded_for;
     proxy_set_header X-Forwarded-Proto \$scheme;
+    proxy_buffering off;
+    proxy_cache off;
+    proxy_read_timeout 86400;
   }"
 fi
 
